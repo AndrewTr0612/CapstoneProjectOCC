cecc02671a161f1ae4e4e1bd94cb098354cd8e85
AndrewTruong <takeiteasy@andrewMac.local>
2025-11-20 13:40:44 -0800

    erge with phillip, alex, andrew

--- headerFiles/BotList.h
+++ headerFiles/BotList.h
@@ -10,8 +10,8 @@ class BotList
 public:
 	void insertBot(BotType& theBot);
 	int getCount() const;
-	bool getBotByName(const string& name, BotType& outBot);
-	bool isEmpty() const;
+	std::bool getBotByName(const std::string& name, BotType& outBot);
+	std::bool isEmpty() const;
 	void printAllBotsNames() const;
 	void printAllBotsInfo() const;
 
