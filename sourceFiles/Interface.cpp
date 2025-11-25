c93cd78c13eb4d69cbe0497422ad0b02a27d4b1c
alexander-shamaan <alexander.shamaan@gmail.com>
2025-11-24 17:37:08 -0800

    Added Interface.h and Interface.cpp skeleton

---
+++ sourceFiles/Interface.cpp
@@ -0,0 +1,48 @@
+/*
+    Shamaan, Alexander (team leader)
+    Cruz, Alejandro
+    Truong, Andrew
+    Truong, Phillip
+
+    Fall 2025
+    CS A250 - C++ 2
+
+    Bot Elections
+*/
+
+#include "Interface.h"
+
+
+#include <iostream>
+#include <string>
+
+
+using namespace std;
+
+
+// Definition function displayMenu
+void displayMenu()
+{
+	// Your code here...
+}
+
+
+// Definition function exitMessage
+void exitMessage()
+{
+	// Your code here...
+}
+
+
+// Definition function noDataMessage
+void noDataMessage()
+{
+	// Your code here...
+}
+
+
+// Definition function processMenu
+void processMenu(const BotList& botList, const Election& election)
+{
+	// Your code here...
+}
